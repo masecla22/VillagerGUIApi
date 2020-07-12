392fc556d78ee61f281117c59f9e1953e9a93162
masecla22 <supermasecla22@gmail.com>
2020-07-12 18:35:51 +0300

    Removed the debug listener

--- src/main/java/masecla/villager/api/MainLoader.java
+++ src/main/java/masecla/villager/api/MainLoader.java
@@ -1,24 +1,10 @@
 package masecla.villager.api;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import masecla.villager.adapters.BaseAdapter;
-import masecla.villager.classes.VillagerInventory;
-import masecla.villager.classes.VillagerTrade;
-import masecla.villager.events.VillagerInventoryCloseEvent;
-import masecla.villager.events.VillagerInventoryModifyEvent;
-import masecla.villager.events.VillagerInventoryOpenEvent;
-import masecla.villager.events.VillagerTradeCompleteEvent;
 
 public class MainLoader extends JavaPlugin {
 
@@ -35,46 +21,6 @@ public class MainLoader extends JavaPlugin {
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | IOException e) {
 			e.printStackTrace();
 		}
-
-		this.getServer().getPluginManager().registerEvents(new Listener() {
-			@EventHandler
-			public void onClose(VillagerInventoryCloseEvent event) {
-				event.getPlayer().sendMessage("You closed your inventory!");
-			}
-
-			@EventHandler
-			public void onModify(VillagerInventoryModifyEvent event) {
-				event.getPlayer().sendMessage("You modified your inventory!");
-			}
-
-			@EventHandler
-			public void onTrade(VillagerTradeCompleteEvent event) {
-				event.getPlayer().sendMessage("You completed a trade");
-			}
-
-			@EventHandler
-			public void onOpen(VillagerInventoryOpenEvent event) {
-				event.getPlayer().sendMessage("Opened an inventory!");
-			}
-
-			@EventHandler
-			public void onChat(PlayerCommandPreprocessEvent event) {
-				if (!event.getMessage().contains("test"))
-					return;
-				List<VillagerTrade> trades = new ArrayList<>();
-				trades.add(new VillagerTrade(new ItemStack(Material.DIRT), new ItemStack(Material.DIRT),
-						new ItemStack(Material.GLASS), 10));
-				trades.add(new VillagerTrade(new ItemStack(Material.DIRT), new ItemStack(Material.GLASS), 15));
-				trades.add(new VillagerTrade(new ItemStack(Material.GLASS), new ItemStack(Material.GLASS), 10));
-
-				VillagerInventory cr = new VillagerInventory(trades, event.getPlayer());
-
-				cr.setName(ChatColor.translateAlternateColorCodes('&', "&4&lfUcKinG TeStInG"));
-
-				cr.open();
-			}
-
-		}, this);
 	}
 
 	@Override
